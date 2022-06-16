0f2381a17346edff869d9212e15c3a1025335876
UncleMadeleine <59909003+UncleMadeleine@users.noreply.github.com>
2022-06-16 10:23:17 +0800

    完成了4-3

---
+++ query4-3(2).py
@@ -0,0 +1,58 @@
+from math import *
+
+
+# 空载时参数
+k = 4080
+hgk = 0.845
+Lk = 3.95
+ak = 2.1
+betak = 0.38
+bk = Lk-ak
+
+# 满载时参数
+mm = 9290
+hgm = 1.17
+Lm = 3.95
+am = 2.95
+betam = 0.38
+bm = Lm-am
+
+z = []
+zmin = 0
+zmax = 1
+zstep = 0.1
+while zmin <= zmax:
+    z.append(zmin)
+    zmin = zmin+zstep
+
+fai = z
+fai_fk = []
+fai_fm = []
+fai_rk = []
+fai_rm = []
+for i in z:
+    fai_fk.append(betak*i*Lk/(bk+i*hgk))
+    fai_fm.append(betam*i*Lm/(bm+i*hgm))
+    fai_rk.append((1-betak)*i*Lk/(ak-i*hgk))
+    fai_rm.append((1-betam)*i*Lm/(am-i*hgm))
+Efk = []
+Efm = []
+Erk = []
+Erm = []
+for i in range(1, len(z)):
+    # print(fai_fk[i])
+    Efk.append(z[i]/fai_fk[i]*100)
+    Efm.append(z[i]/fai_fm[i]*100)
+    Erk.append(z[i]/fai_rk[i]*100)
+    Erm.append(z[i]/fai_rm[i]*100)
+t1=0.02
+t2=0.02
+ua0=30
+fai=0.8
+g=9.8
+# print(len(Erk))
+ak1=Erk[7]*g*fai/100
+am1=Erm[7]*g*fai/100
+Sk1=(t1+t2/2)*ua0/3.6+pow(ua0,2)/(25.92*ak1)
+Sk2=(t1+t2/2)*ua0/3.6+pow(ua0,2)/(25.92*am1)
+print("空载时，汽车制动距离为%.3lfm；满载时，汽车制动距离为%.3lfm" % (Sk1, Sk2))
